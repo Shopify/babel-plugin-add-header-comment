0e7832e860bc692dae39019647fbce041ab92f7a
mikkoh <mikko.haapoja@shopify.com>
2016-08-22 11:30:31 -0400

    Attempting to figure out why circle is not running childProcess.execSync properly

--- test/test-transpile-file.js
+++ test/test-transpile-file.js
@@ -9,7 +9,7 @@ export default function() {
   it('should only add comment to one file', () => {
     const expect = fs.readFileSync(path.join(__dirname, 'expect-transpile-file'), 'utf8');
 
-    const code = childProcess.execSync(`BABEL_ENV=test-plugin node_modules/.bin/babel ${path.join('test', 'toTranspile/')}`, { encoding: 'utf8' });
+    const code = childProcess.execSync('BABEL_ENV=test-plugin node_modules/.bin/babel test/toTranspile/', { encoding: 'utf8' });
 
     assert.equal(code, expect, 'Added comment only to one file');
   });
